2cf7200e64e682fd85730ef4768738ccf0c3c2a7
Radosław Korotkiewicz <git@ezioleq.com>
2026-08-19 20:27:05 +0200

    Add Open Graph support

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+/** Site title. */
+export const SITE_TITLE = "ezioleq";
+/** Fallback description used by the homepage and any page without its own. */
+export const SITE_DESCRIPTION =
+  "Dive into the river of my thoughts - they aren't always entirely correct, but hey, this is my personal blog. Go ahead, take a look!";
+/** Open Graph embed image. */
+export const OG_IMAGE_PATH = "/favicon.png";
